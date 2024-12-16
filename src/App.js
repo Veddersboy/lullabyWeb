8f53d4313827d138827776b2657c0a76bb57cc53
Nick Fallon <veddersboy2005@gmail.com>
2024-12-16 00:20:30 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <a
           // className="App-link"
           // href="https://reactjs.org"
           // target="_blank"
           // rel="noopener noreferrer"
         >
+          <p>
+          This is a test
+          </p>
         </a>
       </header>
     </div>
